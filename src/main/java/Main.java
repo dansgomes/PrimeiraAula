152129800ed3a3ba83900cf3223679bf7ba91c7e
dansgomes <danielsgomes99@gmail.com>
2023-08-30 18:49:37 -0300

    Nome alterado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import com.primeiraaula.service.PersonService;
 public class Main {
     public static void main(String[] args) {
         PersonService personService = new PersonService();
-        personService.newPerson("Daniel", 24, true);
+        personService.newPerson("Rafael", 24, true);
     }
 }
