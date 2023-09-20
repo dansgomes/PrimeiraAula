d3091fb933c685fd68e72ff29ba1af0ce6f22915
Daniel Gomes <danielsgomes99@gmail.com>
2023-09-20 19:10:07 -0300

    Adicionado novos parâmetros  para a classe Rh, com objetivo de calcular seu salário baseado na modalidade. Adicionado também testes para comprovar a funcionalidade do código (ainda com bugs)

--- src/main/java/com/primeiraaula/domain/Rh.java
+++ src/main/java/com/primeiraaula/domain/Rh.java
@@ -1,16 +1,68 @@
 package com.primeiraaula.domain;
+import java.util.Scanner;
 
 public class Rh {
+    public String getModalidade() {
+        return modalidade;
+    }
 
-    public void teste(){
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+
+    private String modalidade;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    private double salario;
+
+    public void teste() {
         System.out.println("Teste");
     }
 
-    public void calcularSalario(String modalidade, double salario){
-        System.out.println(modalidade + " : "  + salario);
+    public void calcularSalario(String modalidade, double salario) {
 
-
+        System.out.println(modalidade + " : " + salario);
 
     }
 
+    public static void pjSalario() {
+        double salario = 800;
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe quantas horas trabalhadas.");
+        int horasTrabalhadas = ler.nextInt();
+        double novoSalarioPJ = salario + 10 * horasTrabalhadas;
+
+        System.out.println("Seu salário é: " + novoSalarioPJ);
+        // Método que calcula o salário do PJ
+    }
+
+    public static void cltSalario(String[] args) {
+        double salario = 1000;
+        double novoSalarioCLT;
+        novoSalarioCLT = salario * 1.08;
+
+        System.out.println("Seu salário é: " + novoSalarioCLT);
+        // Método que calcula o salário do CLT
+    }
+
+    public static void estagioSalario() {
+        double salario = 500;
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe quantos dias trabalhados.");
+        int diasTrabalhados = ler.nextInt();
+        double novoSalarioEstagio = salario + 10 * diasTrabalhados;
+
+        System.out.println("Seu salário é: " + novoSalarioEstagio);
+        // Método que calcula o salário do Estágio
+    }
 }
+
--- src/test/java/com/primeiraaula/service/SalarioTest.java
+++ src/test/java/com/primeiraaula/service/SalarioTest.java
@@ -4,17 +4,47 @@ package com.primeiraaula.service;
 import com.primeiraaula.domain.Rh;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class SalarioTest {
 
     @Test
-    public void testarSalario(){
+    public void testarSalario() {
 
         Rh rh = new Rh();
 
         rh.teste();
         rh.calcularSalario("CLT", 1000.00);
-        rh.calcularSalario("PJ", 500.00);
-        rh.calcularSalario("ESTAGIO", 800.00);
+        rh.calcularSalario("PJ", 800.00);
+        rh.calcularSalario("ESTAGIO", 500.00);
 
+        Scanner entrada = new Scanner(System.in);
+
+        boolean calcularSalario = true;
+
+        while (calcularSalario) {
+            System.out.println("Escolha uma modalidade");
+            System.out.println("[1] PJ");
+            System.out.println("[2] CLT");
+            System.out.println("[3] Estagio");
+            System.out.println("[4] Parar");
+
+            int escolha = entrada.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.println(Rh.pjSalario());
+                    break;
+                case 2:
+                    System.out.println(Rh.cltSalario());
+                    break;
+                case 3:
+                    System.out.println(Rh.estagioSalario());
+                    break;
+                case 4:
+                    return;
+            }
+
+        }
     }
 }
